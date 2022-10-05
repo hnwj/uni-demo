5394b244bd598457634ee9eaed1754ac9dafa842
hnwj <2948662591@qq.com>
2022-10-05 12:05:06 +0800

    完成了 home 首页的开发

--- main.js
+++ main.js
@@ -2,20 +2,53 @@ import App from './App'
 
 // #ifndef VUE3
 import Vue from 'vue'
+// 按需导入 $http 对象
+import {
+	$http
+} from '@escook/request-miniprogram'
+uni.$http = $http
+$http.baseUrl = 'https://api-ugo-web.itheima.net'
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中...',
+	})
+}
+
+// 请求完成之后做一些事情，关闭数据加载
+$http.afterRequest = function() {
+	uni.hideLoading()
+}
+
+// 封装 uni.$showMsg() 方法
+uni.$showMsg = function(title = "数据加载失败！", duration = 1500) {
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none'
+	})
+}
+
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
 // #endif
 
+
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,41 @@
 <template>
 	<view>
-		home
+		<!-- 轮播图区域 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<swiper-item v-for="item in swiperList" :key="item.goods_id">
+				<navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id=' +item.goods_id">
+					<image :src="item.image_src" mode=""></image>
+				</navigator>
+			</swiper-item>
+		</swiper>
+		<!-- 分类导航区域 -->
+		<view class="nav-list">
+			<view class="nav-item" v-for="(item,index) in navList" :key="index">
+				<image :src="item.image_src" mode="" class="nav-img" @click="navClickHandler(item)"></image>
+			</view>
+		</view>
+		<!-- 楼层数据 -->
+		<view class="floor-list">
+			<!-- 楼层item -->
+			<view class="floor-item" v-for="(item,index) in floorList" :key="index">
+				<image :src="item.floor_title.image_src" mode="" class="floor-title"></image>
+				<!-- 楼层图片区域 -->
+				<view class="floor-img-box">
+					<!-- 左侧 -->
+					<navigator class="left-img-box" :url="item.product_list[0].url">
+						<image :src="item.product_list[0].image_src" mode="widthFix"
+							:style="{width:item.product_list[0].image_width+'rpx'}"></image>
+					</navigator>
+					<!-- 右侧 -->
+					<view class="right-img-box">
+						<navigator class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-if="i2 !== 0" :url="item2.url">
+							<image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width + 'rpx'}">
+							</image>
+						</navigator>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +43,114 @@
 	export default {
 		data() {
 			return {
-				
+				// 轮播图数据
+				swiperList: [],
+				// 分类导航的数据列表
+				navList: [],
+				// 楼层数据
+				floorList: [],
 			};
+		},
+		onLoad() {
+			this.getswiperList()
+			this.getNavList()
+			this.getFloorList()
+		},
+		methods: {
+			// 获取轮播图的数据
+			async getswiperList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				// 数据请求失败
+				if (res.meta.status !== 200) return uni.$showMsg()
+				// 数据请求成功
+				this.swiperList = res.message
+				console.log('this.swiperList', this.swiperList);
+			},
+			// 获取分层数据
+			async getNavList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/catitems')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				this.navList = res.message
+				console.log('this.navList', this.navList);
+			},
+			// 获取楼层数据
+			async getFloorList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/floordata')
+				if (res.meta.status !== 200) return uni.$showMsg()
+				// 通过双层for循环，处理url地址
+				res.message.forEach(floor => {
+					floor.product_list.forEach(prod => {
+						prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1]
+					})
+				})
+				this.floorList = res.message
+				console.log('this.floorList', this.floorList);
+			},
+			// 路由跳转
+			navClickHandler(item) {
+				if (item.name === '分类') {
+					uni.switchTab({
+						url: '/pages/cate/cate'
+					})
+				}
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 330rpx;
+	}
 
+	.swiper-item,
+	image {
+		width: 100%;
+		height: 100%;
+	}
+
+	.nav-list {
+		display: flex;
+		justify-content: space-around;
+		margin: 15px 0;
+
+		.nav-img {
+			width: 128rpx;
+			height: 140rpx;
+		}
+	}
+
+	.floor-title {
+		height: 60rpx;
+		width: 100%;
+		display: flex;
+	}
+
+	.right-img-box {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+	}
+
+	.floor-img-box {
+		display: flex;
+		padding-left: 10rpx;
+	}
+
+	.right-img-box {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+	}
+
+	.floor-img-box {
+		display: flex;
+		padding-left: 10rpx;
+	}
 </style>
